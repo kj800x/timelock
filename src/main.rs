92ae03b1e07ffde66e793484bca998f6cb23e3d7
Kevin Johnson <kevin@coolkev.com>
2020-05-22 09:57:49 -0400

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -24,43 +24,50 @@ fn hash(iv: [u8; 32], stopped: &AtomicBool) -> ([u8; 32], u64) {
     return (bytes, i);
 }
 
-fn main() {
-    let stopped = Arc::new(AtomicBool::new(false));
-    let stopped1 = Arc::clone(&stopped);
+type ThreadResult = (u8, [u8; 32], u64, [u8; 32]);
 
-    const THREADS: u8 = 12;
+fn generate_work(threads: u8) -> Vec<ThreadResult> {
+    let stopped = Arc::new(AtomicBool::new(false));
+    let handler_stopped_flag = Arc::clone(&stopped);
 
     ctrlc::set_handler(move || {
-        stopped.swap(true, Ordering::Relaxed);
+        handler_stopped_flag.swap(true, Ordering::Relaxed);
     })
     .expect("Error setting Ctrl-C handler");
-
-    println!("Hello, world!");
 
     let mut seed = [0u8; 32];
     OsRng.fill_bytes(&mut seed);
     let mut rng = ChaCha20Rng::from_seed(seed);
 
-    let mut join_handles: Vec<thread::JoinHandle<(u8, [u8; 32], u64, [u8; 32])>> = Vec::new();
+    let mut join_handles: Vec<thread::JoinHandle<ThreadResult>> = Vec::new();
 
-    for thread_index in 0..THREADS {
-        let shared1 = Arc::clone(&stopped1);
+    for thread_index in 0..threads {
+        let thread_stopped_flag = Arc::clone(&stopped);
         let mut initial_value = [0u8; 32];
         rng.fill(&mut initial_value);
         join_handles.push(thread::spawn(move || {
-            let (hash, count) = hash(initial_value, &shared1);
+            let (hash, count) = hash(initial_value, &thread_stopped_flag);
             (thread_index, initial_value, count, hash)
         }))
     }
 
-    println!("");
-    for join in join_handles {
-        let (thread_index, initial_value, count, hash) =
-            join.join().expect("Error joining the threads");
+    join_handles
+        .into_iter()
+        .map(|join_handle| join_handle.join().expect("Error joining the threads"))
+        .collect()
+}
 
+fn print_results(results: Vec<ThreadResult>) {
+    println!("");
+    for (thread_index, initial_value, count, hash) in results {
         println!(
-            "Thread {}: {} iterations\n\tInitial Seed: {:x?}\n\tResult Hash: {:x?}\n",
+            "Thread {}: {} iterations\n\tInitial Seed: {:x?}\n\tResult Hash: {:x?}",
             thread_index, count, initial_value, hash
         );
     }
+}
+
+fn main() {
+    let results = generate_work(8);
+    print_results(results);
 }
